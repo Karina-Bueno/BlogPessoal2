599c8593bd9f24f339db27487628235547a52e75
Karina Bueno <buenogkarina@gmail.com>
2022-05-03 18:58:57 -0300

    Implementando a segurança do projeto, login e cadastro funcionando corretamente, consumindo a API

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,11 @@
+import { Tema } from "./Tema"
+import { Usuario } from "./Usuario"
+
+export class Postagem {
+  public id: number
+  public titulo: string
+  public texto: string
+  public data: Date
+  public usuarios: Usuario //importando do Usuario.ts
+  public tema: Tema //importando do Tema.ts
+}
