b8176df1d4b71d79be2a89059115c7bc44a1237b
Karina Bueno <buenogkarina@gmail.com>
2022-05-05 21:46:13 -0300

    Iserindo o componente inicio

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -46,4 +46,6 @@ export class CadastrarComponent implements OnInit {
 
   }
 
+  
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 import { AuthService } from '../service/auth.service';
 
@@ -24,8 +25,18 @@ export class LoginComponent implements OnInit {
   login() {
     this.auth.login(this.usuarioLogin).subscribe((resp: UsuarioLogin) => {
       this.usuarioLogin = resp
+
+      environment.token = this.usuarioLogin.token
+      environment.nome = this.usuarioLogin.nome
+      environment.foto = this.usuarioLogin.foto
+      environment.id = this.usuarioLogin.id
+
       this.router.navigate(["/inicio"])
-    });
+    }, erro => {
+      if (erro.status == 500) {
+        alert("Usuário ou senha estão incorretos")
+      }
+    })
 
   }
 
